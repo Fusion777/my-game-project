d3706e48c73806bb5e8fbc12a0acd519ff12c02b
Fusion777 <mywonderfulmailbox@gmail.com>
2023-06-03 13:16:01 -0400

    Add a function to check if a chosen(clicked) letter exists in the known word

---
+++ main.js
@@ -0,0 +1,41 @@
+/*  Global variables:
+    1. const word -> original hidden word that needs to be guessed;
+    2. let guessLetter -> one of the letters to choose from, if a 
+    current letter clicked exists in the original word, the corresponding
+    square shows that letter or letters, if there are more than one such
+    letter in the word; that letter becomes inactive in the letters
+    to choose; the number of guesses left decreases by one;
+    3. let guessWord -> a word that user inputs as a guess, if that word
+    corresponds to the original word, the user wins;
+    4. constLetters(array) -> after user clicks a letter, the corresponding
+    button becomes inactive;
+    5. guessCount -> number of guesses left */
+    
+/*  Global functions:
+    1. checkWin -> checks if the const guessWord corresponds to the original
+    word; if yes, the user wins and the game ends; if no, counter
+    decreases left guesses by one and game waits for next guessLetter
+    or guessWord. If no more guesses left in the counter, the user loses.
+    2. deactivateLetter -> the chosen letters or buttons become inactive;
+    3. countDown -> decreases the number of guesses, until it reaces zero.
+    When zero guesses left the user loses and the game ends. */
+
+    // const word;
+    // let guessLetter;
+    // let guessWord;
+    // const letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+    // let guessCount;
+    
+    const word = "VORTEX";
+    const buttons = document.querySelectorAll("button");
+    let resultEl = document.getElementById("result");
+    buttons.forEach(function(button) {
+        button.addEventListener("click", function() {
+            let buttonLetter = this.innerText;
+            if (word.includes(buttonLetter)) {
+                resultEl.innerText = `The letter ${buttonLetter} exists in the word!`;
+            } else {
+                resultEl.innerText = `The word has no letter ${buttonLetter}, try again!`;
+            }
+        })
+    })
